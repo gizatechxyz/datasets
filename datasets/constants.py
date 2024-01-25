c593ff8e9a6af3462b4b77e50e7f253e716d7925
Alejandro Martinez <alejandromartinez@Alejandros-MacBook-Pro.local>
2024-01-25 10:18:09 +0100

    add bucket path and some tags

--- datasets/constants.py
+++ datasets/constants.py
@@ -2,25 +2,31 @@ from datasets.models import Dataset
 
 DATASET_HUB = [
     Dataset(
-        name="dataset1",
-        path="your-bucket/path/to/dataset1.parquet",
+        name="TVL_fee_per_protocol",
+        path="gs://datasets-giza/TVL_fee_per_protocol/",
         description="Description for dataset1",
-        labels=["label1"],
+        labels=["aggregated", "daily", "TVL", "fees"], #TODO
         documentation="Documentation for dataset1",
     ),
     Dataset(
-        name="dataset2",
-        path="your-bucket/path/to/dataset2.parquet",
+        name="TVL_per_project_tokens",
+        path="gs://datasets-giza/TVL_per_project_tokens",
         description="Description for dataset2",
-        labels=["label1", "label2"],
+        labels=["aggregated", "daily", "TVL"], #TODO
         documentation="Documentation for dataset2",
     ),
-    ## Invalid labeling label3 for testing
         Dataset(
-        name="dataset3",
-        path="your-bucket/path/to/dataset3.parquet",
+        name="tokens_OHCL",
+        path="gs://datasets-giza/tokens_OHCL",
         description="Description for dataset3",
-        labels=["label1", "label3"],    
+        labels=["aggregated", "daily", "TVL"], #TODO
+        documentation="Documentation for dataset3",
+    ),
+        Dataset(
+        name="top_pools_APY_per_protocol",
+        path="gs://datasets-giza/top_pools_APY_per_protocol",
+        description="Description for dataset3",
+        labels=["aggregated", "daily", "TVL"],  #TODO
         documentation="Documentation for dataset3",
     ),
 ]
