0ae2d8596abdcee369652bda43c5d5bfcaa36224
Alejandro Martinez <alejandromartinez@Alejandros-MacBook-Pro.local>
2024-01-30 11:21:13 +0100

    delete lazy test

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,19 +6,10 @@ from giza_datasets import DatasetLoader
 
 def test_load_dataset_eager():
     loader = DatasetLoader()
-    df = loader.load("dataset1", eager=True)
+    df = loader.load("dataset1")
     assert isinstance(df, pl.DataFrame)
-
-
-def test_load_dataset_lazy():
-    loader = DatasetLoader()
-    df = loader.load("dataset1", eager=False)
-    if not isinstance(df, pl.DataFrame):
-        df = df.collect()
-    assert isinstance(df, pl.DataFrame)
-
 
 def test_dataset_not_in_hub():
     loader = DatasetLoader()
     with pytest.raises(ValueError):
-        loader.load("non_existent_dataset", eager=True)
+        loader.load("non_existent_dataset")
