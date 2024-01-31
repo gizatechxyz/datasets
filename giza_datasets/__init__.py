60ea1246fc517d3282a3be801bdee517707347af
Alejandro Martinez <alejandromartinez@Alejandros-MacBook-Pro.local>
2024-01-31 17:23:51 +0100

    add CacheManager

--- giza_datasets/__init__.py
+++ giza_datasets/__init__.py
@@ -1,4 +1,5 @@
 from giza_datasets.hub import DatasetsHub
 from giza_datasets.loaders import DatasetsLoader
+from giza_datasets.cache_manager import CacheManager
 
 __all__ = ["DatasetsLoader", "DatasetsHub"]
